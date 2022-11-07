4769dc215198a114bafb2ae238a02f3f717231f7
edenxx <yidanxu9818@gmail.com>
2022-11-07 14:29:13 -0500

    add pairwise test

--- yidan_fruit_ripening.R
+++ yidan_fruit_ripening.R
@@ -2,12 +2,13 @@ library(ggplot2)
 library(tibble)
 library(tidyr)
 
-df = read.csv("df_melted.csv")
+df = read.csv("g_df_melted.csv")
 df$number = as.factor(df$number)
 df$treatment = as.factor(df$treatment)
 df$covariates = as.numeric(as.factor(df$covariates))
 
-grn_df = df[13:72,]
+# grn_df = df[13:72,]
+grn_df = df
 
 g = ggplot(grn_df, aes(x=covariates, y=value, color=number)) +
     facet_wrap(~treatment, ncol=4) +
@@ -15,30 +16,31 @@ g = ggplot(grn_df, aes(x=covariates, y=value, color=number)) +
     # geom_smooth(se=FALSE, method=loess) +
     geom_line() +
     labs(x = "Time point", y = "Lag 1 Difference of Measurements") +
-    ggtitle("Normalised Green RGB Value") +
+    # ggtitle("Green RGB Value") +
+    ggtitle("Green RGB Value") +
     theme_classic() 
 
 hist_g = ggplot(grn_df) +
     # facet_wrap(~covariates, ncol=5) +
     geom_histogram(aes(x=value, color=factor(covariates), fill=factor(covariates)), alpha=0.5, position="dodge") +
-    labs(x = "Lag 1 Difference of Normalised Green RGB") +
+    labs(x = "Lag 1 Difference of RGB Magnitude") +
     ggtitle("Histogram for each Day of Measurements") +
     labs(colour = "Day", fill="Day") +
     theme_classic() 
 
-soft_df = df[1:12,]
-soft_g = ggplot(soft_df, aes(x=treatment, y=value, color=number, fill=number, size=-value)) +
-    # facet_wrap(~treatment, ncol=4) +
-    # geom_violin(alpha=0.7) + 
-    geom_point(position=position_dodge(0.5), alpha=0.7) +
-    # geom_smooth(se=FALSE, method=loess) +
-    # geom_line() +
-    labs(x = "Treatment", y = "Difference of Softness") +
-    ggtitle("Softness") +
-    theme_classic() 
+# soft_df = df[1:12,]
+# soft_g = ggplot(soft_df, aes(x=treatment, y=value, color=number, fill=number, size=-value)) +
+#     # facet_wrap(~treatment, ncol=4) +
+#     # geom_violin(alpha=0.7) + 
+#     geom_point(position=position_dodge(0.5), alpha=0.7) +
+#     # geom_smooth(se=FALSE, method=loess) +
+#     # geom_line() +
+#     labs(x = "Treatment", y = "Difference of Softness") +
+#     ggtitle("Softness") +
+#     theme_classic() 
 
 ggsave("./figs/green.png", plot=g)
-ggsave("./figs/soft.png", plot=soft_g)
-ggsave("./figs/hist.png", plot=hist_g)
+# ggsave("./figs/soft.png", plot=soft_g)
+ggsave("./figs/mag_hist.png", plot=hist_g)
 
 
--- yidan_fruit_ripening.py
+++ yidan_fruit_ripening.py
@@ -14,7 +14,7 @@ def _median(val, perm_trt, idx_trt, nsize):
 
 	out = jnp.zeros_like(idx_trt, dtype=jnp.float32)
 	for i in range(len(idx_trt)):
-		idx = jnp.argwhere(perm_trt == i, size=nsize) # 3 bananas for 5 days
+		idx = jnp.argwhere(perm_trt == idx_trt[i], size=nsize) # 3 bananas for 5 days
 		out = out.at[i].set(jnp.median(val.at[idx].get()))
 	return out
 
@@ -29,7 +29,7 @@ def var_median(val, perm_trt, idx_trt, nsize):
 	den = 0.
 
 	for i in range(len(idx_trt)):
-		idx = jnp.argwhere(perm_trt == i, size=nsize)
+		idx = jnp.argwhere(perm_trt == idx_trt[i], size=nsize)
 		sqrt = (val.at[idx].get() - local_med.at[i].get())**2
 		den += jnp.sum(sqrt)
 
@@ -56,6 +56,9 @@ def _perm(key, trt, idx_trt, val, stats, days=5):
 		perm_stats = jnp.max(jnp.abs(_median(val, perm_trt, idx_trt, 3*days)))
 	elif stats == "mean_median":
 		perm_stats = jnp.mean(jnp.abs(_median(val, perm_trt, idx_trt, 3*days)))
+	elif stats == "diff_median":
+		med =  _median(val, perm_trt, idx_trt, 3*days)
+		perm_stats = med[1] - med[0]
 	else:
 		raise NotImplementedError
 
@@ -101,7 +104,9 @@ if __name__ == "__main__":
 	# normalise the green with magnitude
 	g_banana = banana.loc[:, g_col]
 	mag_banana = banana.loc[:, mag_col]
-	gnorm_banana = pd.DataFrame(g_banana.values/mag_banana.values, columns=col_names[g_col])
+	# gnorm_banana = pd.DataFrame(g_banana.values/mag_banana.values, columns=col_names[g_col])
+	gnorm_banana = g_banana
+	# gnorm_banana = mag_banana
 	gnorm_diff_banana = gnorm_banana.diff(periods=1, axis=1) # remove trend
 	soft_diff = data.iloc[:,2:4].diff(periods=1, axis=1)
 	
@@ -111,43 +116,49 @@ if __name__ == "__main__":
 	g_df_melted = pd.melt(g_df, id_vars=["number", "treatment"], var_name="covariates", value_name="value")
 
 	# # save the entirety of df
-	# df = pd.concat([data.iloc[:,:2], soft_diff.iloc[:,1:], gnorm_diff_banana.iloc[:,1:]], axis=1)
-	# df_melted = pd.melt(df, id_vars=["number", "treatment"], var_name="covariates", value_name="value")
-	# # print(df_melted)
-	# df_melted.to_csv("df_melted.csv")
+	df = pd.concat([data.iloc[:,:2], soft_diff.iloc[:,1:], gnorm_diff_banana.iloc[:,1:]], axis=1)
+	df_melted = pd.melt(df, id_vars=["number", "treatment"], var_name="covariates", value_name="value")
+	# print(df_melted)
+	df_melted.to_csv("mag_df_melted.csv")
+	g_df_melted.to_csv("g_df_melted.csv")
 
 	########################## Stats based on Median 5 days ############################
-	g_val = jnp.asarray(g_df_melted["value"].values) # vector 12*5=72
-	obs_trt = g_df["treatment"].values # vector 12
-	idx_trt = jnp.unique(obs_trt).astype(jnp.int32)
-	n_trt = len(idx_trt)
+	# g_val = jnp.asarray(g_df_melted["value"].values) # vector 12*5=72
+	# obs_trt = g_df["treatment"].values # vector 12
+	# idx_trt = jnp.unique(obs_trt).astype(jnp.int32)
+	# n_trt = len(idx_trt)
+	# print(g_df_melted)
+	# print("gval", g_val)
+	# m = 10000
+	# key = jrm.PRNGKey(130)
+	# stats = "var_median"
+	# days = 5
+	# perm_stats = perm(key, m, obs_trt, idx_trt, g_val, stats, days=days)
+	# # print(g_df_melted["treatment"].values)
+	# # print(_perm(key, obs_trt, idx_trt, g_val, stats))
+	# if stats == "var_median":
+	# 	obs_stats = var_median(g_val, g_df_melted["treatment"].values, idx_trt, nsize=3*days)
+	# 	# title = "Variance of Between Treatment Medians/Within Treatment Medians"
+	# 	title = "Consecutive Change in RGB Magnitude"
+	# elif stats == "max_median":
+	# 	obs_stats = jnp.max(jnp.abs(_median(g_val, g_df_melted["treatment"].values, idx_trt, nsize=3*days)))
+	# 	title = "Max of Absolute Medians"
+	# elif stats == "mean_median":
+	# 	obs_stats = jnp.mean(jnp.abs(_median(g_val, g_df_melted["treatment"].values, idx_trt, nsize=3*days)))
+	# 	title = "Mean of Absolute Medians"
+	# else:
+	# 	raise NotImplementedError
 
-	m = 1000
-	key = jrm.PRNGKey(130)
-	stats = "mean_median"
-	perm_stats = perm(key, m, obs_trt, idx_trt, g_val, stats, days=5)
-	# print(g_df_melted["treatment"].values)
-	# print(_perm(key, obs_trt, idx_trt, g_val, stats))
-	if stats == "var_median":
-		obs_stats = var_median(g_val, g_df_melted["treatment"].values, idx_trt, nsize=3*5)
-		title = "Variance of Between Treatment Medians/Within Treatment Medians"
-	elif stats == "max_median":
-		obs_stats = jnp.max(jnp.abs(_median(g_val, g_df_melted["treatment"].values, idx_trt, nsize=3*5)))
-		title = "Max of Absolute Medians"
-	elif stats == "mean_median":
-		obs_stats = jnp.mean(jnp.abs(_median(g_val, g_df_melted["treatment"].values, idx_trt, nsize=3*5)))
-		title = "Mean of Absolute Medians"
-	else:
-		raise NotImplementedError
+	# print("obs stats {}".format(stats), obs_stats)
 
-	pval = jnp.mean(perm_stats >= obs_stats)
+	# pval = jnp.mean(perm_stats >= obs_stats)
 
-	plt.figure(figsize=(12, 7))
-	sns.histplot(np.asarray(perm_stats), kde=True)
-	plt.axvline(obs_stats, color="r", label="observed")
-	plt.xlabel("Test Stats under Sharp Null (p value = {})".format(pval))
-	plt.title(title)
-	plt.savefig("./figs/Ftest_5_{}.png".format(stats))
+	# plt.figure(figsize=(12, 7))
+	# sns.histplot(np.asarray(perm_stats), kde=True)
+	# plt.axvline(obs_stats, color="r", label="observed")
+	# plt.xlabel("Test Stats under Sharp Null (p value = {})".format(pval))
+	# plt.title(title)
+	# plt.savefig("./figs/5_mag_{}.png".format(stats))
 
 	############################# mean across days #####################################
 
@@ -158,10 +169,11 @@ if __name__ == "__main__":
 	# print(obs_trt)
 	# print(g_val_mean)
 
-	# m = 1000
+	# m = 10000
 	# key = jrm.PRNGKey(130)
-	# stats = "max_median"
-	# perm_stats = perm(key, m, obs_trt, idx_trt, g_val_mean, stats, days=1)
+	# stats = "var_median"
+	# days = 1
+	# perm_stats = perm(key, m, obs_trt, idx_trt, g_val_mean, stats, days=days)
 	# # print(g_df_melted["treatment"].values)
 	# # print(_perm(key, obs_trt, idx_trt, g_val, stats))
 
@@ -169,10 +181,10 @@ if __name__ == "__main__":
 	# 	obs_stats = var_median(g_val_mean, g_df_melted["treatment"].values, idx_trt, nsize=3*1)
 	# 	title = "Variance of Between Treatment Medians/Within Treatment Medians"
 	# elif stats == "max_median":
-	# 	obs_stats = jnp.max(jnp.abs(_median(g_val_mean, g_df_melted["treatment"].values, idx_trt, nsize=3*1)))
+	# 	obs_stats = jnp.max(jnp.abs(_median(g_val_mean, g_df_melted["treatment"].values, idx_trt, nsize=3*days)))
 	# 	title = "Max of Absolute Medians"
 	# elif stats == "mean_median":
-	# 	obs_stats = jnp.mean(jnp.abs(_median(g_val_mean, g_df_melted["treatment"].values, idx_trt, nsize=3*1)))
+	# 	obs_stats = jnp.mean(jnp.abs(_median(g_val_mean, g_df_melted["treatment"].values, idx_trt, nsize=3*days)))
 	# 	title = "Mean of Absolute Medians"
 	# else:
 	# 	raise NotImplementedError
@@ -184,7 +196,94 @@ if __name__ == "__main__":
 	# plt.axvline(obs_stats, color="r", label="observed")
 	# plt.xlabel("Test Stats under Sharp Null (p value = {})".format(pval))
 	# plt.title(title)
-	# plt.savefig("./figs/Ftest_1_{}.png".format(stats))
+	# plt.savefig("./figs/1_mag_{}.png".format(stats))
+
+
+	############################ difference of median compare to control #######################
+
+	g_val = jnp.asarray(g_df_melted["value"].values) # vector 12*5=60
+	obs_trt = jnp.asarray(g_df_melted["treatment"].values) # vector 12*5=60
+	m = 10000
+	key = jrm.PRNGKey(130)
+	stats = "diff_median"
+	days = 5
+
+	p_val = []
+
+	# control vs. apple, cucumber, cucumber + apple
+	for i in [1,2,3]:
+		g_val_2 = g_val[(obs_trt==0) + (obs_trt==i)]
+		obs_trt_2 = obs_trt[(obs_trt==0) + (obs_trt==i)]
+		idx_trt_2 = jnp.array([0,i])
+
+		perm_stats = perm(key, m, obs_trt_2[:6], idx_trt_2, g_val_2, stats, days=days)
+		med = _median(g_val_2, obs_trt_2, idx_trt_2, nsize=3*days)
+		obs_stats = med[1] - med[0]
+
+		p_val.append(jnp.mean(obs_stats >= perm_stats))
+
+
+	# apple vs. cucumber, cucumber + apple
+	for i in [0,2,3]:
+		g_val_2 = g_val[(obs_trt==1) + (obs_trt==i)]
+		obs_trt_2 = obs_trt[(obs_trt==1) + (obs_trt==i)]
+		idx_trt_2 = jnp.array([1,i])
+
+		perm_stats = perm(key, m, obs_trt_2[:6], idx_trt_2, g_val_2, stats, days=days)
+		med = _median(g_val_2, obs_trt_2, idx_trt_2, nsize=3*days)
+		obs_stats = med[1] - med[0]
+
+		p_val.append(jnp.mean(obs_stats >= perm_stats))
+
+
+	for i in [0,1,3]:
+		g_val_2 = g_val[(obs_trt==2) + (obs_trt==i)]
+		obs_trt_2 = obs_trt[(obs_trt==2) + (obs_trt==i)]
+		idx_trt_2 = jnp.array([2,i])
+
+		perm_stats = perm(key, m, obs_trt_2[:6], idx_trt_2, g_val_2, stats, days=days)
+		med = _median(g_val_2, obs_trt_2, idx_trt_2, nsize=3*days)
+		obs_stats = med[1] - med[0]
+
+		p_val.append(jnp.mean(obs_stats >= perm_stats))
+
+	for i in [0,1,2]:
+		g_val_2 = g_val[(obs_trt==3) + (obs_trt==i)]
+		obs_trt_2 = obs_trt[(obs_trt==3) + (obs_trt==i)]
+		idx_trt_2 = jnp.array([3,i])
+
+		perm_stats = perm(key, m, obs_trt_2[:6], idx_trt_2, g_val_2, stats, days=days)
+		med = _median(g_val_2, obs_trt_2, idx_trt_2, nsize=3*days)
+		obs_stats = med[1] - med[0]
+
+		p_val.append(jnp.mean(obs_stats >= perm_stats))
+
+
+	# # cucumber vs cucumber + apple
+	# g_val_2 = g_val[(obs_trt==2) + (obs_trt==3)]
+	# obs_trt_2 = obs_trt[(obs_trt==2) + (obs_trt==3)]
+	# idx_trt_2 = jnp.array([2,3])
+
+	# perm_stats = perm(key, m, obs_trt_2[:6], idx_trt_2, g_val_2, stats, days=days)
+	# med = _median(g_val_2, obs_trt_2, idx_trt_2, nsize=3*days)
+	# obs_stats = med[1] - med[0]
+
+	# p_val.append(jnp.mean(obs_stats >= perm_stats))
+
+
+
+	print(obs_stats)
+	print(perm_stats)
+	print(p_val)
+	# output
+	# [DeviceArray(0.3924, dtype=float32), DeviceArray(0.5022, dtype=float32), DeviceArray(0.4021, dtype=float32), DeviceArray(0.65389997, dtype=float32), DeviceArray(0.3425, dtype=float32), DeviceArray(1., dtype=float32)]
+
+
+	
+	# perm_stats = perm(key, m, obs_trt, idx_trt, g_val_mean, stats, days=days)
+	# obs_stats = var_median(g_val, g_df_melted["treatment"].values, idx_trt, nsize=3*days)
+	# print(g_df_melted["treatment"].values)
+	# print(_perm(key, obs_trt, idx_trt, g_val, stats))
 
 
 
